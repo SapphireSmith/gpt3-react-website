c61f79b2a6aaf70e336b95ff91388f62326a931e
Sapphire Smith <ssapphire527@gmail.com>
2023-03-31 19:52:57 +0530

    Navbar design completed @responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import {CTA, Brand, Navbar} from './components'
+import {Blog,Feature,Footer,Header,Possibility,WhatGPT3} from './containers'
+import './App.css'
 const App = () => {
   return (
-    <div>GPT</div>
-  )
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Feature/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
+    </div>
+    )
 }
 
 export default App
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const whatGPT3 = () => {
+  return (
+    <div>whatGPT3</div>
+  )
+}
+
+export default whatGPT3
